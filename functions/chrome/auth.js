f6d8e8df396c246501a8dddfcc1ff05a971b4c87
Peter Schilleman <peter@eenengelswoord.nl>
2019-09-02 15:46:30 +0200

    auth (WIP)

--- functions/chrome/auth.js
+++ functions/chrome/auth.js
@@ -1,9 +1,7 @@
 const api_key = process.env.API_KEY;
 
 module.exports = (event, context) => {
-  console.log('::headers::')
-  console.log(JSON.stringify(event.headers))
-  if (event.headers.Authorization !== 'undefined' && event.headers.Authorization === api_key) {
+  if (event.headers.authorization !== 'undefined' && event.headers.authorization === api_key) {
     return true;
   }
   console.log('unauthorized')
