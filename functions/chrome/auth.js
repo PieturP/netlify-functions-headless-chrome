fe2b2463ee2e0c7ebf0ff7e056bb3a56c22c8420
Peter Schilleman <peter@eenengelswoord.nl>
2019-09-02 15:40:32 +0200

    auth tryout

--- functions/chrome/auth.js
+++ functions/chrome/auth.js
@@ -1,7 +1,7 @@
 const api_key = process.env.API_KEY;
 
 module.exports = (event, context) => {
-  if (event.queryStringParameters.api_key !== 'undefined' && event.queryStringParameters.api_key == api_key) {
+  if (event.headers.Authorization !== 'undefined' && event.headers.Authorization === api_key) {
     return true;
   }
   console.log('unauthorized')
