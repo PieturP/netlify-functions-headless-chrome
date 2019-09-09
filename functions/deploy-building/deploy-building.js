ff516768187c65445e3375177319545f14d4fcf8
Peter Schilleman <peter@eenengelswoord.nl>
2019-09-09 15:51:47 +0200

    tryout

---
+++ functions/deploy-building/deploy-building.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+console.log('building...');
+
+const date = new Date().toString();
+const url = `https://hooks.zapier.com/hooks/catch/3416786/o3zmw51/?date=${date}&description=building&source=script`;
+
+
+axios.get(url)
+
+exports.handler = async (event, context) => {
+  console.log('buidling 2')
+  axios.get(url)
+}
