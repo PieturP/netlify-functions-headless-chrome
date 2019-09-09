f2ea460582ab371afc6e9c7e89b643cac2d23cdc
Peter Schilleman <peter@eenengelswoord.nl>
2019-09-09 16:14:43 +0200

    sdfdsf

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 
-const url = `https://hooks.zapier.com/hooks/catch/3416786/o3zmw51/?date=today&description=building&source=script`;
-
 
 // axios.get(url)
 
 exports.handler = async (event, context) => {
   console.log('buidling 2')
+  const url = `https://hooks.zapier.com/hooks/catch/3416786/o3zmw51/?date=123&description=456&source=script`
+
   axios.get(url).then((response) => console.log(JSON.stringify(response)))
   console.log('bulding 3')
 }
