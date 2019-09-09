a7791b23be3cccc0ae553635c004fbc51d5374c1
Peter Schilleman <peter@eenengelswoord.nl>
2019-09-09 15:36:11 +0200

    tryout

---
+++ functions/deploy-succeeded.js
@@ -0,0 +1,34 @@
+const request = require('request');
+console.log('succeeded');
+
+
+const url = `https://hooks.zapier.com/hooks/catch/3416786/o3zmw51/?date=${date}&description=succeeded&source=script`;
+
+  const options = {json: true};
+
+
+
+  request(url, options, (error, res, body) => {
+      if (error) {
+          return  console.log(error)
+      };
+  });
+
+//https://hooks.zapier.com/hooks/catch/3416786/o3zmw51/?date=123&description=456&source=script
+exports.handler = async (event, context, callback) => {
+  console.log('deploy started') ;
+
+  const date = new Date().toString();
+  const url = `https://hooks.zapier.com/hooks/catch/3416786/o3zmw51/?date=${date}&description=succeeded&source=script`;
+
+  const options = {json: true};
+
+
+
+  request(url, options, (error, res, body) => {
+      if (error) {
+          return  console.log(error)
+      };
+  });
+
+}
