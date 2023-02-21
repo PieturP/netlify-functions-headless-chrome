3679db45d2725f8c23d92b452265645e8bb76d52
Peter Schilleman <peter@eenengelswoord.nl>
2023-02-21 13:54:47 +0100

    auth disabled for now

--- functions/chrome/auth.js
+++ functions/chrome/auth.js
@@ -1,14 +1,16 @@
 const api_key = process.env.API_KEY;
 
 module.exports = (event, context) => {
+
   return true;
-  console.log('::headers::')
-  console.log(JSON.stringify(event.headers))
-  if (event.headers.authorization !== 'undefined' && event.headers.authorization === api_key) {
-    return true;
-  }
-  console.log('unauthorized')
-  console.log(JSON.stringify(event))
-  console.log(JSON.stringify(context))
-  return false;
+
+  // console.log('::headers::')
+  // console.log(JSON.stringify(event.headers))
+  // if (event.headers.authorization !== 'undefined' && event.headers.authorization === api_key) {
+  //   return true;
+  // }
+  // console.log('unauthorized')
+  // console.log(JSON.stringify(event))
+  // console.log(JSON.stringify(context))
+  // return false;
 }
